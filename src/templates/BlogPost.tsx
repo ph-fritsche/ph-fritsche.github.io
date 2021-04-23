3f6fd103f452cd4840912ed44c4dc7f99a0a643a
Philipp Fritsche <ph.fritsche@gmail.com>
2021-04-23 20:49:51 +0200

    wip: fix BlogPost

--- src/templates/BlogPost.tsx
+++ src/templates/BlogPost.tsx
@@ -1,45 +1,43 @@
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
-import { graphql } from 'gatsby'
+import { graphql, PageProps } from 'gatsby'
 import { useDarkModeSwitch } from '~src/components/App/Config'
 import * as mdxComponents from './mdxComponents'
 import { CardContent, CardHeader } from '@material-ui/core'
 import Card from '~src/components/Card'
 
-export interface blogPostFrontmatter {
-  title?: string
-  tags?: string[],
-  date?: string,
-}
-
-export default function BlogPost({mdx}: GatsbyTypes.BlogPostQuery) {
+export default function BlogPost({data}: PageProps<GatsbyTypes.BlogPostQuery>) {
     useDarkModeSwitch()
 
     return (
         <Card component="article">
             <CardHeader
-                title={mdx?.meta.title}
+                title={data.mdx?.meta.title}
                 titleTypographyProps={{variant: 'h1'}}
-                subheader={mdx?.meta.date && (new Date(mdx.meta.date)).toLocaleDateString()}
+                subheader={data.mdx?.meta.date && (new Date(data.mdx.meta.date)).toLocaleDateString()}
             />
             <CardContent>
-                <MDXProvider components={mdxComponents}>
-                    <MDXRenderer>{JSON.stringify(mdx?.body ?? '')}</MDXRenderer>
-                </MDXProvider>
+                {
+                    data.mdx?.body && (
+                        <MDXProvider components={mdxComponents}>
+                            <MDXRenderer>{data.mdx.body}</MDXRenderer>
+                        </MDXProvider>
+                    )
+                }
             </CardContent>
         </Card>
     )
 }
 
 export const pageQuery = graphql`
-  query BlogPost($id: String) {
-    mdx(id: { eq: $id }) {
-      body
-      meta {
-        title
-        date
-      }
+    query BlogPost($id: String) {
+        mdx(id: { eq: $id }) {
+            body
+            meta {
+                title
+                date
+            }
+        }
     }
-  }
 `
