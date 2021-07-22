32479502993b379a6f36f2ae3fc7a78f85d893bf
Philipp Fritsche <ph.fritsche@gmail.com>
2021-07-22 10:12:00 +0000

    fix: lazy load App

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,16 +1,9 @@
-import React from 'react'
-import { Box, CssBaseline, ThemeProvider } from '@material-ui/core'
+import React, { Suspense } from 'react'
 import { Helmet } from 'react-helmet'
 
-import AppBar from './AppBar'
-import Footer from './Footer'
-import { ConfigProvider } from './Config'
-import useTheme from './theme'
-import useBackground from './background'
-import { Swipeable } from '../Swipeable'
-import { css } from '@emotion/react'
+const App = React.lazy(() => import('./App'))
 
-export default function App({
+export default function AppWrapper({
     children,
 }: React.PropsWithChildren<unknown>) {
     return <div key="App">
@@ -29,66 +22,10 @@ export default function App({
                 rel="stylesheet"
             />
         </Helmet>
-        <ConfigProvider>
-            <AppInConfig>{children}</AppInConfig>
-        </ConfigProvider>
+        <Suspense fallback={null}>
+            <App>
+                {children}
+            </App>
+        </Suspense>
     </div>
-}
-
-function AppInConfig({
-    children,
-}: React.PropsWithChildren<unknown>) {
-    const theme = useTheme()
-
-    return (
-        <ThemeProvider theme={theme}>
-            <CssBaseline />
-            <AppInTheme>{children}</AppInTheme>
-        </ThemeProvider>
-    )
-}
-
-function AppInTheme({
-    children,
-}: React.PropsWithChildren<unknown>) {
-    const theme = useTheme()
-
-    const bgSx = useBackground()
-
-    const bodyCss = css({
-        fontFamily: `'Alegreya Sans', sans-serif`,
-        fontWeight: 100,
-        margin: 0,
-        color: theme.palette.getContrastText(theme.palette.background.default),
-        minHeight: '100vh',
-        ...bgSx,
-    })
-
-    return (
-        <Swipeable>
-            <Box sx={{
-                display: 'flex',
-                flexDirection: 'column',
-                minHeight: '100vh',
-            }}>
-                <Helmet
-                    bodyAttributes={{
-                        class: `css-${bodyCss.name}`,
-                    }}
-                >
-                    <style>{`.css-${bodyCss.name} { ${bodyCss.styles} }`}</style>
-                </Helmet>
-                <AppBar sx={bgSx}/>
-                <Box component="main" sx={{
-                    flex: 1,
-                    display: 'flex',
-                    flexDirection: 'column',
-                    justifyContent: 'center',
-                }}>
-                    {children}
-                </Box>
-                <Footer/>
-            </Box>
-        </Swipeable>
-    )
 }
